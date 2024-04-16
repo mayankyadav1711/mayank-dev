9a69d7a3e860affbb6930f3ed61316368955fc81
mayankyadav1711 <mykyadav2003@gmail.com>
2024-04-16 17:02:17 +0530

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import { Syne } from "@next/font/google";
+import type { Metadata } from "next";
+import { Syne } from "next/font/google";
 
 
 const syne = Syne({
@@ -8,6 +9,62 @@ const syne = Syne({
   display: "block",
   weight: ["400", "500", "600", "700", "800"],
 });
+
+export const metadata: Metadata = {
+  title: "Mayank Yadav — MERN Stack Developer",
+  description:
+    "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
+  generator: "Next.js",
+  applicationName: "Mayank Yadav",
+  keywords: [
+    "freelance", "developer", "frontend", "react", "creative", "tech", "ahmedabad", "software", "portfolio", "mern stack developer", "backend developer", "web development", "javascript", "typescript", "node.js", "express.js", "mongodb", "database", "restful API", "graphql", "programming", "coding", "web design", "user interface", "user experience", "product design", "technology", "remote work", "software engineering", "coding enthusiast", "digital solutions", "problem solver", "innovative solutions", "agile development", "code quality", "mayank yadav", "myk yadav", "yadav mayank", "mayank ldrp", "mayank nptel", "yadav nptel", "myk yadav", "mayank ssip", "mayank collegpt", "mayank yadav collegpt", "yadav mayank collegpt", "collegpt founder", "collegpt developer", "mayank founder collegpt", "mayank developer collegpt", "collegpt mayank yadav", "mayank yadav safarnamaaa", "mayank yadav mongodb", "yadav mayank ldrp", "yadav ldrp", "ldrpmayank", "yadav ldrp", "ldrpyadav", "collegpt yadav", "yadav collegpt", "mayank mern", "yadav mern", "mayank developer", "mayank mern stack", "mayank yadav mern", "mayank yadav react", "mayank react", "mayank hackingly"
+
+  ],
+  colorScheme: "dark",
+  openGraph: {
+    title: "Mayank Yadav — MERN Stack Developer",
+    description:
+      "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
+    url: "https://mayank-dev.vercel.app/",
+    siteName: "https://mayank-dev.vercel.app/",
+    images: [
+      {
+        url: "https://i.ibb.co/jkLVTTR/image.png",
+        width: 1200,
+        height: 630,
+        alt: "Mayank Yadav — MERN Stack Developer",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mayank Yadav — MERN Stack Developer",
+    description:
+      "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
+    creator: "mayankyadav_17",
+
+    images: [
+      "https://i.ibb.co/jkLVTTR/image.png",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
+};
+
 type RootLayoutProps = {
   children: ReactNode;
 };
@@ -19,6 +76,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
       >
         {children}
+ 
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
-import Head from 'next/head';
-import NavBar from "./navbar/NavBar";
 import Hero from "./hero-section/Hero";
-import Work from "./work-section/Work";
-import About from "./about-section/About";
-import Blog from "./blog-section/BlogGrid";
-import Contact from "./contact-section/Contact";
-import Footer from "./footer/Footer";
-import PreLoader from "./animations/PreLoader/PreLoader";
 import useBlobity from "blobity/lib/react/useBlobity";
 import { useEffect } from "react";
+import { ScrollerMotion } from "scroller-motion";
+import PreLoader from "./animations/PreLoader/PreLoader";
 import { initialBlobityOptions } from "./utils/BlobityConfig";
+import NavBar from "./navbar/NavBar";
+
+import dynamic from "next/dynamic";
+const Work = dynamic(() => import("./work-section/Work"));
+const About = dynamic(() => import("./about-section/About"));
+const Blog = dynamic(() => import("./blog-section/BlogGrid"));
+const Contact = dynamic(() => import("./contact-section/Contact"));
+const Footer = dynamic(() => import("./footer/Footer"));
 
 export default function Home() {
   const blobityInstance = useBlobity(initialBlobityOptions);
@@ -31,45 +33,20 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* Metadata */}
-        <title>Mayank Yadav</title>
-        <meta name="description" content="Founder & Developer at ColleGPT || MERN STACK Developer || React Js Developer || Student at LDRP Institute of Technology & Research." />
-        {/* Add other meta tags as needed */}
-        {/* Keywords */}
-        <meta name="keywords" content="freelance, developer, frontend, react, creative, tech, ahmedabad, software, portfolio, mern stack developer, backend developer, web development, javascript, typescript, node.js, express.js, mongodb, database, restful API, graphql, programming, coding, web design, user interface, user experience, product design, technology, remote work, software engineering, coding enthusiast, digital solutions, problem solver, innovative solutions, agile development, code quality, mayank yadav, myk yadav, yadav mayank, mayank ldrp, mayank nptel, yadav nptel, myk yadav, mayank ssip, mayank collegpt, mayank yadav collegpt, yadav mayank collegpt, collegpt founder, collegpt developer, mayank founder collegpt, mayank developer collegpt, collegpt mayank yadav, mayank yadav safarnamaaa, mayank yadav mongodb, yadav mayank ldrp, ldrp mayank, yadav ldrp, ldrp yadav, collegpt yadav, yadav collegpt, mayank mern, yadav mern, mayank developer, mayank mern stack, mayank yadav mern, mayank yadav react, mayank react, mayank hackingly" />
-        
-        {/* Open Graph */}
-        <meta property="og:title" content="Mayank Yadav" />
-        <meta property="og:description" content="Founder & Developer at ColleGPT || MERN STACK Developer || React Js Developer || Student at LDRP Institute of Technology & Research." />
-        <meta property="og:image" content="https://i.ibb.co/jkLVTTR/image.png" />
-        <meta property="og:url" content="https://mayank-dev.vercel.app/" /> {/* Replace with your website URL */}
-
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en-US" />
-        
-        {/* LinkedIn */}
-        <meta property="linkedin:title" content="Mayank Yadav" />
-        <meta property="linkedin:description" content="Founder & Developer at ColleGPT || MERN STACK Developer || React Js Developer || Student at LDRP Institute of Technology & Research." />
-        <meta property="linkedin:image" content="https://i.ibb.co/QDqRccS/image.png" />
-
-      </Head>
-
       <PreLoader />
 
       <NavBar />
 
+      {/* <ScrollerMotion> */}
       <main className="flex flex-col items-center justify-center">
         <Hero />
         <Work />
         <About />
-        <Blog/>
+        <Blog />
         <Contact />
         <Footer />
       </main>
+      {/* </ScrollerMotion> */}
     </>
   );
 }
