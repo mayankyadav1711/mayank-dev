795dae24359be8a386a93ce7dc59c11283a47620
mayankyadav1711 <mykyadav2003@gmail.com>
2024-04-16 16:41:08 +0530

    merge conflict

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -133,50 +133,50 @@ export const devProjects = [
   },
 ];
 
-// export const designProjects = [
-//   {
-//     id: 1,
-//     name: "Hebron Statup Lab Website",
-//     description:
-//       "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
-//     technologies: ["UX Research", "UI Design", "Prototyping"],
-//     github: "",
-//     demo: "",
-//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
-//     available: false,
-//   },
-//   {
-//     id: 2,
-//     name: "RAGS Scrubs Website",
-//     description:
-//       "An image generator website that allows users to generate, combine, and download images.",
-//     technologies: ["UX Research", "UI Design", "Prototyping"],
-//     github: "",
-//     demo: "",
-//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
-//     available: false,
-//   },
-//   {
-//     id: 3,
-//     name: "Crown Branding Agency Website",
-//     description:
-//       "A website that reduces the length of your URL using Bit.ly's API",
-//     technologies: ["UX Research", "UI Design", "Prototyping"],
-//     github: "",
-//     demo: "",
-//     image:
-//       "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
-//     available: false,
-//   },
-//   {
-//     id: 4,
-//     name: "Titi Mobile App",
-//     description:
-//       "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
-//     technologies: ["UX Research", "UI Design", "Prototyping"],
-//     github: "",
-//     demo: "",
-//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
-//     available: false,
-//   },
-// ];
+export const designProjects = [
+  {
+    id: 1,
+    name: "Hebron Statup Lab Website",
+    description:
+      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 2,
+    name: "RAGS Scrubs Website",
+    description:
+      "An image generator website that allows users to generate, combine, and download images.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 3,
+    name: "Crown Branding Agency Website",
+    description:
+      "A website that reduces the length of your URL using Bit.ly's API",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image:
+      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 4,
+    name: "Titi Mobile App",
+    description:
+      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
+    available: false,
+  },
+];
